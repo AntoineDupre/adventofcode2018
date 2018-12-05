d73992b81eced0794c5e36b191908b858be38184
AntoineDupre <dupre.antoine.niclas@gmail.com>
2018-12-05 20:59:42 +0100

    Day1 part2

--- rust/day1/src/main.rs
+++ rust/day1/src/main.rs
@@ -1,15 +1,35 @@
 use std::io;
 use std::fs;
+use std::collections::HashSet;
 
-fn day1(f: &str) -> i32 {
+
+fn resulting_frequency(f: &str) -> i32 {
     f.lines().map(|v: &str| -> i32 {v.parse().expect("not an int")}).sum()
+}
+
+
+fn frequency(f: &str){
+    let mut s = HashSet::new();
+    let mut current = 0;
+    for l in f.lines().cycle().map(|v: &str| -> i32 {v.parse().expect("not an int")}){
+        current += l;
+        if s.contains(&current){
+            println!("{}", current);
+            break;
+        };
+        s.insert(current);
+    };
 }
 
 fn read_file(file_name: &str) -> Result<String, io::Error> {
     fs::read_to_string(file_name)
 }
 
+
+
 fn main() {
     let f = read_file("input/input").expect("Cannot read file");
-    println!("day1: {}",day1(&f));
+    println!("Resulting frequency {}",resulting_frequency(&f));
+    println!("First frequency reached twice:");
+    frequency(&f);
 }
